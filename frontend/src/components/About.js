52791c6de02e8c73c17eb30f2260b49cf0844503
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-08-02 14:59:23 +1000

    update: finished aws cloud practitioner

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -4,12 +4,11 @@ const About = () => {
     return (
         <div className="about">
             <h2>Some things I've been spending my time on recently...</h2>
+            <p>Just finished the AWS Cloud Practioner Cert, now working towards AWS Solutions Architect Associate</p>
             <p>Hosting trivia nights with friends (which motivated me to deploy Trivia Night!) </p>
             <p>Learning Japanese and trying out conlangs like Toki Pona through translating simple texts</p>
             <p>Designing games, usually focused on strategy or social deduction mechanics</p>
-            <p>Working towards the AWS Solutions Architect Associate Cert</p>
-            <p>Dipping my toes into VR development and Apache Kafka to for my next project</p>
-            <p>Collecting random facts ^.^</p>
+            <p>Dipping my toes into VR & Game development</p>
         </div>
     );
 }
